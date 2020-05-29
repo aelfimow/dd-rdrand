b0d372aa7d279dccde143e1f81d2dc661f34941b
Alexander Elfimow <my.free.mail@gmx.de>
2020-05-29 14:04:45 +0200

    Added further rdrand functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <stdexcept>
 #include <vector>
 #include <cstring>
+#include <map>
+#include <functional>
 
 #include "rdrand_func.h"
 #include "argv_parser.h"
@@ -71,8 +73,31 @@ catch (...)
 
 static void rand_fill(std::vector<size_t> &buffer)
 {
-    for (auto &value: buffer)
+    static std::map<size_t, std::function<void (size_t *p)>> const factory
     {
-        rdrand_func1(&value);
+        { 4,    [](size_t *p) { rdrand_func4(p);    } },
+        { 8,    [](size_t *p) { rdrand_func8(p);    } },
+        { 16,   [](size_t *p) { rdrand_func16(p);   } },
+        { 32,   [](size_t *p) { rdrand_func32(p);   } },
+        { 64,   [](size_t *p) { rdrand_func64(p);   } },
+        { 128,  [](size_t *p) { rdrand_func128(p);  } },
+        { 256,  [](size_t *p) { rdrand_func256(p);  } },
+        { 512,  [](size_t *p) { rdrand_func512(p);  } },
+        { 1024, [](size_t *p) { rdrand_func1024(p); } },
+    };
+
+    auto it = factory.find(buffer.size());
+
+    if (it == factory.end())
+    {
+        for (auto &value: buffer)
+        {
+            rdrand_func1(&value);
+        }
+    }
+    else
+    {
+        auto &func = it->second;
+        func(buffer.data());
     }
 }
